e92c12b8d11fbc76e1a12bb2640cdbe11e63f2ff
gisnaefde <gisnaefde198@gmail.com>
2022-10-10 17:37:57 +0700

    Properties Type Declaration

--- Properties.php
+++ Properties.php
@@ -4,20 +4,20 @@ require_once 'data/Person.php';
 
 $Properties = new Person(); //Untuk memanggil Properties yang ada di class person gunakan tanda -> kemudian nama properties nya.
 $Properties->name = "Gisna Fauzian Dermawan";
-$Properties->address = "Tasikmalaya";
-$Properties->email="gisna@gmail.com";
+$Properties->address = "Kuala Lumpur";
+$Properties->country="Malaysia";
 
 echo "Name : $Properties->name".PHP_EOL;
 echo "Address : $Properties->address".PHP_EOL;
-echo "Email : $Properties->email".PHP_EOL;
+echo "Country : $Properties->country".PHP_EOL;
 
 $Properties2 = new Person();
 $Properties2->name = "Adinda";
 $Properties2->address = "Jakarta";
-$Properties2->email = "adinda@gmail.com";
+$Properties2->country = "Indonesia";
 
 echo "Name : $Properties2->name".PHP_EOL;
 echo "address : $Properties2->address".PHP_EOL;
-echo "Email : $Properties2->email".PHP_EOL;
+echo "Country : $Properties2->country".PHP_EOL;
 
 ?>
