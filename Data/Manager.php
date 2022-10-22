24463969b49e74e6f0d56cb79d8eb33ec019b8c2
gisnaefde <gisnaefde198@gmail.com>
2022-10-22 17:21:06 +0700

    Constructor Overriding

--- Data/Manager.php
+++ Data/Manager.php
@@ -4,6 +4,14 @@ class Manager
 {
     var string $name;
 
+    var string $title;
+
+    public function __construct(string $name = "", string $title = "Manager")
+    {
+        $this->name = $name;
+        $this->title = $title;
+    }
+
     function sayHello(string $name){
         echo "Haii $name, my name is Manager $this->name".PHP_EOL;
     }
@@ -11,6 +19,12 @@ class Manager
 
 class VicePresident extends Manager //class ini merupakan turunan dari class Manager, meski tidak ada isi, sebenarnya data nya ada karena mewarisi dari manager
 {
+    public function __construct(string $name = "")
+    {
+        //tidak wajib tapi di rekomendasikan
+        parent::__construct($name , "VicePresident" );
+    }
+
     function sayHello(string $name){
         echo "Haii $name, my name is Vice President $this->name".PHP_EOL;
     }
